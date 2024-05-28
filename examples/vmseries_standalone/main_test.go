1016a1b359b2bc2b977d53c01752460562b64e0e
Slawomir Kaczmarek <skaczmarek@paloaltonetworks.com>
2024-05-28 09:42:30 +0200

    Add IPv6 example tests

--- examples/vmseries_standalone/main_test.go
+++ examples/vmseries_standalone/main_test.go
@@ -8,14 +8,14 @@ import (
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
 
-func CreateTerraformOptions(t *testing.T) *terraform.Options {
+func CreateTerraformOptions(t *testing.T, varFiles []string) *terraform.Options {
 	// prepare random prefix
 	randomNames, _ := testskeleton.GenerateTerraformVarsInfo("aws")
 
 	// define options for Terraform
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		TerraformDir: ".",
-		VarFiles:     []string{"example.tfvars"},
+		VarFiles:     varFiles,
 		Vars: map[string]interface{}{
 			"name_prefix":  randomNames.NamePrefix,
 			"ssh_key_name": "test-ssh-key",
@@ -34,28 +34,43 @@ func TestValidate(t *testing.T) {
 }
 
 func TestPlan(t *testing.T) {
+	// IPv4
 	// define options for Terraform
-	terraformOptions := CreateTerraformOptions(t)
+	terraformOptionsIpv4 := CreateTerraformOptions(t, []string{"example.tfvars"})
 	// prepare list of items to check
 	assertList := []testskeleton.AssertExpression{}
 	// plan test infrastructure and verify outputs
-	testskeleton.PlanInfraCheckErrors(t, terraformOptions, assertList, "No errors are expected")
+	testskeleton.PlanInfraCheckErrors(t, terraformOptionsIpv4, assertList, "No errors are expected")
+
+	// IPv6
+	terraformOptionsIpv6 := CreateTerraformOptions(t, []string{"example_ipv6.tfvars"})
+	testskeleton.PlanInfraCheckErrors(t, terraformOptionsIpv6, assertList, "No errors are expected")
 }
 
 func TestApply(t *testing.T) {
+	// IPv4
 	// define options for Terraform
-	terraformOptions := CreateTerraformOptions(t)
+	terraformOptionsIpv4 := CreateTerraformOptions(t, []string{"example.tfvars"})
 	// prepare list of items to check
 	assertList := []testskeleton.AssertExpression{}
 	// deploy test infrastructure and verify outputs and check if there are no planned changes after deployment
-	testskeleton.DeployInfraCheckOutputs(t, terraformOptions, assertList)
+	testskeleton.DeployInfraCheckOutputs(t, terraformOptionsIpv4, assertList)
+
+	// IPv6
+	terraformOptionsIpv6 := CreateTerraformOptions(t, []string{"example_ipv6.tfvars"})
+	testskeleton.DeployInfraCheckOutputs(t, terraformOptionsIpv6, assertList)
 }
 
 func TestIdempotence(t *testing.T) {
+	// IPv4
 	// define options for Terraform
-	terraformOptions := CreateTerraformOptions(t)
+	terraformOptionsIpv4 := CreateTerraformOptions(t, []string{"example.tfvars"})
 	// prepare list of items to check
 	assertList := []testskeleton.AssertExpression{}
 	// deploy test infrastructure and verify outputs and check if there are no planned changes after deployment
-	testskeleton.DeployInfraCheckOutputsVerifyChanges(t, terraformOptions, assertList)
+	testskeleton.DeployInfraCheckOutputsVerifyChanges(t, terraformOptionsIpv4, assertList)
+
+	//IPv6
+	terraformOptionsIpv6 := CreateTerraformOptions(t, []string{"example_ipv6.tfvars"})
+	testskeleton.DeployInfraCheckOutputsVerifyChanges(t, terraformOptionsIpv6, assertList)
 }
