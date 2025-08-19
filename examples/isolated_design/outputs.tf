50fd272450d0743cb3e403149960614006ba9d65
Slawomir Kaczmarek <skaczmarek@paloaltonetworks.com>
2025-08-19 15:27:55 +0200

    Refactor isolated design example

--- examples/isolated_design/outputs.tf
+++ examples/isolated_design/outputs.tf
@@ -5,18 +5,17 @@ output "vmseries_public_ips" {
   value       = { for k, v in module.vmseries : k => v.public_ips }
 }
 
-##### App VPC #####
-
+##### Spokes ALB & NLB #####
 output "application_load_balancers" {
   description = <<-EOF
   FQDNs of Application Load Balancers
   EOF
-  value       = { for k, v in module.public_alb : k => v.lb_fqdn }
+  value       = { for k, v in module.app_alb : k => v.lb_fqdn }
 }
 
 output "network_load_balancers" {
   description = <<-EOF
   FQDNs of Network Load Balancers.
   EOF
-  value       = { for k, v in module.public_nlb : k => v.lb_fqdn }
+  value       = { for k, v in module.app_nlb : k => v.lb_fqdn }
 }
