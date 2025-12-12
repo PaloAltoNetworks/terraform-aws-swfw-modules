083ee5978f92ca5fd90fdfd20e4a953460a4b57c
Lukasz Stadnik <lstadnik@paloaltonetworks.com>
2025-12-12 12:58:08 +0100

    feat(modules/cloudngfw: add new capabilities link_id

--- modules/cloudngfw/outputs.tf
+++ modules/cloudngfw/outputs.tf
@@ -2,3 +2,8 @@ output "cloudngfw_service_name" {
   description = "The service endpoint name exposed to tenant environment."
   value       = cloudngfwaws_ngfw.this.endpoint_service_name
 }
+
+output "fw_id" {
+  description = "Id of the Cloud NGFW resource"
+  value       = cloudngfwaws_ngfw.this.firewall_id
+}
